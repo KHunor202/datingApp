76be671f8e4ca79651f83a77b212ae3cc8415dbe
Kolozsi Hunor <hkolozsi@abesse.hu>
2025-07-09 17:30:59 +0200

    end of section 4

---
+++ API/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs;
+
+public class UserDTO
+{
+    public string Id { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;   
+    public string DisplayName { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; } = string.Empty;
+}
